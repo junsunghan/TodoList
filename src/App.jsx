4d661dd3dc491f8e12b3deb60f3d9b64f21b8717
junsunghan <hjs8213073@gmail.com>
2024-10-28 11:58:58 +0900

    2. UI 구현완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import List from "./components/List";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
 
 function App() {
-  return <></>;
+  return (
+    <div className="APP">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,12 @@
+import "./Editor.css";
+
 const Editor = () => {
-  return <div></div>;
+  return (
+    <div className="Editor">
+      <input placeholder="새로운 Todo..." />
+      <button>추가</button>
+    </div>
+  );
 };
 
 export default Editor;
