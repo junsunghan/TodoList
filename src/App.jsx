ed099a700e19cf5775beba9d1a3bff6850eefe55
junsunghan <hjs8213073@gmail.com>
2024-10-29 11:15:49 +0900

    5. CheckBox 수정 기능

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { useState, useRef } from "react";
 const mockData = [
   {
     id: 0,
-    isDone: false,
+    isDone: true,
     content: "React 공부",
     date: new Date().getTime(),
   },
@@ -39,11 +39,23 @@ function App() {
     setTodos([newTodo, ...todos]);
   };
 
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에
+    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+
+    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
     <div className="APP">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} />
+      <List todos={todos} onUpdate={onUpdate} />
     </div>
   );
 }
